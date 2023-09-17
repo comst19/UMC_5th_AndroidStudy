608f3a44d2f28dd580499430be1a2c0b2db9d0ce
comst19 <comst19@naver.com>
2023-09-17 19:46:30 +0900

    Jetpack BottomNavigation 연결 완료

--- Chapter1/app/src/main/java/com/comst/chapter1/MainActivity.kt
+++ Chapter1/app/src/main/java/com/comst/chapter1/MainActivity.kt
@@ -2,10 +2,19 @@ package com.comst.chapter1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.comst.chapter1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        NavigationUI.setupWithNavController(binding.navBottomBar, findNavController(R.id.nav_host))
     }
 }
