b7874c1834f19e304a79a0c8721b382bc4099bcd
comst19 <comst19@naver.com>
2023-09-21 00:03:36 +0900

    바텀 네비게이션 템플릿 세팅

---
+++ FLO_CloneApp/app/src/main/java/com/comst/flo_cloneapp/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.comst.flo_cloneapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.comst.flo_cloneapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
