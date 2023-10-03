6c2b6df5239f238b5f91f0e48add4fb1c8189734
comst19 <comst19@naver.com>
2023-10-03 23:22:13 +0900

    Splash api를 이용해서 Splah 화면 구현

---
+++ FLOCloneApp/app/src/main/java/com/comst/flocloneapp/ui/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.comst.flocloneapp.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.comst.flocloneapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+}
