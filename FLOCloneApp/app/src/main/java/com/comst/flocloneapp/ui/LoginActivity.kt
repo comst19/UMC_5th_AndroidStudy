e00112339d9fc23c4416fb3b18c2e7eaae60c3aa
comst19 <comst19@naver.com>
2023-11-18 13:14:10 +0900

    8주차 미션 UI

---
+++ FLOCloneApp/app/src/main/java/com/comst/flocloneapp/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.comst.flocloneapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.comst.flocloneapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ FLOCloneApp/app/src/main/java/com/comst/flocloneapp/ui/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.comst.flocloneapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.comst.flocloneapp.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
